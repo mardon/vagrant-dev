47e19b25b852621cd23b48eed253e909f8509979
MamuSha <mar.don@seznam.cz>
2014-03-05 12:19:01 +0100

    mysql-server settings

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ sudo apt-get install -y vim
 # cURL
 apt-get install -y curl
 
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
