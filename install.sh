7ce9c20ed7ef344bc649b800a2737e0b6bcf2f48
MamuSha <mar.don@seznam.cz>
2014-03-05 12:36:23 +0100

    install mysql, git

--- install.sh
+++ install.sh
@@ -10,3 +10,12 @@ apt-get install -y curl
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
+# Mysql
+# -----
+# Ignore the post install questions
+apt-get install -y mysql-server-5.5
+
+# Git
+# ---
+apt-get install git-core
+
